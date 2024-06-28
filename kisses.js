235839c00db65743c4778fd18800d961ef4090a5
Napman22 <57738038+Napman22@users.noreply.github.com>
2024-06-28 16:09:45 -0700

    Add files via upload

--- kisses.js
+++ kisses.js
@@ -13,8 +13,32 @@ const phrases = [
     "My lover",
     "I love you because you are you. You bring so much joy to others i wish you could hang out with yourself because then you  would understand the joy you bring",
 ];
+
+const images = [
+    "https://favim.com/pd/s12/gif_previews/7/730/7302/73027/7302708.gif",
+    "https://i.imgur.com/yyzW1xZ.gif",
+    "https://64.media.tumblr.com/e599c49e7a3087c1482cdc32b8f54c65/48c30bae661c6fbe-4c/s540x810/9b574ba7e99fb28219074dbfd72e728482370819.gifv",
+];
+
+function generateContent() {
+    generatePhrase();
+    generateImage();
+}
+
 function generatePhrase() {
+    
     const phraseContainer = document.getElementById("phraseContainer");
     let randomIndex = Math.floor(Math.random() * phrases.length);
     phraseContainer.textContent = phrases[randomIndex];
+}
+
+
+function generateImage() {
+    const imageContainer = document.getElementById("imageContainer");
+    let randomIndex = Math.floor(Math.random() * images.length);
+    const img = document.createElement("img");
+    img.src = images[randomIndex];
+    img.alt = "Random Image";
+    imageContainer.innerHTML = "";
+    imageContainer.appendChild(img);
 }
